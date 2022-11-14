6482103cc4f4336330b7534097e94bb29ea9ac66
Pramod singh <pmdsingh50@gmail.com>
2022-11-14 15:31:53 +0530

    Init Commit - Recipe Application

---
+++ src/main/java/com/dev/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,17 @@
+package com.dev.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppApplication.class, args);
+		System.out.println("Devinder Singh");
+	}
+}
+
+ 
